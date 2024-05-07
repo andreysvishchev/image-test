cffc0e69987bfa5b147c63cebfc3e13461f37f9d
Andrey Svishchev <andrey.svishchev@yandex.ru>
2024-05-07 13:01:01 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,128 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from "next/image"
+import styles from "./page.module.css"
+import img2x_1 from "@/assets/2x.jpeg"
+import img2x_2 from "@/assets/2x-2.jpeg"
+import img1_5x_1 from "@/assets/1.5x.jpeg"
+import img1_5x_2 from "@/assets/1.5x-2.jpeg"
+import img1x_1 from "@/assets/1x.jpeg"
+import img1x_2 from "@/assets/1x-2.jpeg"
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className={styles.main}>
+            <div className={styles.block}>
+                <div className={styles.title}>2x back</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={'https://api.nvg8.ru/upload/tmp/test/2x.jpeg'}
+                        alt=""
+                    />
+                </div>
+                <div className={styles.title}>2x local</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={img2x_1}
+                        alt=""
+                    />
+                </div>
+            </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <div className={styles.block}>
+                <div className={styles.title}>1.5x back</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={'https://api.nvg8.ru/upload/tmp/test/1.5x.jpeg'}
+                        alt=""
+                    />
+                </div>
+                <div className={styles.title}>1.5x local</div>
+                <div className={styles.image}>
+                    <Image
+                        fill
+                        src={img1_5x_1}
+                        alt=""
+                    />
+                </div>
+            </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            <div className={styles.block}>
+                <div className={styles.title}>1x back</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={'https://api.nvg8.ru/upload/tmp/test/1x.jpeg'}
+                        alt=""
+                    />
+                </div>
+                <div className={styles.title}>1x local</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={img1x_1}
+                        alt=""
+                    />
+                </div>
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <div className={styles.block}>
+                <div className={styles.title}>2x back</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={'https://api.nvg8.ru/upload/tmp/test/2x-2.jpeg'}
+                        alt=""
+                    />
+                </div>
+                <div className={styles.title}>2x local</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={img2x_2}
+                        alt=""
+                    />
+                </div>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+            <div className={styles.block}>
+                <div className={styles.title}>1.5x back</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={'https://api.nvg8.ru/upload/tmp/test/1.5x-2.jpeg'}
+                        alt=""
+                    />
+                </div>
+                <div className={styles.title}>1.5x local</div>
+                <div className={styles.image}>
+                    <Image
+                        fill
+                        src={img1_5x_2}
+                        alt=""
+                    />
+                </div>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            <div className={styles.block}>
+                <div className={styles.title}>1x back</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={'https://api.nvg8.ru/upload/tmp/test/1x-2.jpeg'}
+                        alt=""
+                    />
+                </div>
+                <div className={styles.title}>1x local</div>
+                <div className={styles.image}>
+                <Image
+                        fill
+                        src={img1x_2}
+                        alt=""
+                    />
+                </div>
+            </div>
+        </main>
+    )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    trailingSlash: true,
+    images: {
+		domains: [ 'https://api.nvg8.ru', 'api.nvg8.ru'],
+		unoptimized: true,
+	},
+    async redirects() {
+        return [
+            {
+                source: "/bitrix/",
+                destination: "https://#SITE_URL#/bitrix/",
+                statusCode: 301
+            }
+        ]
+    },
+    async headers() {
+        const headers = []
+
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" || process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+            headers.push({
+                headers: [
+                    {
+                        key: "X-Robots-Tag",
+                        value: "noindex"
+                    }
+                ],
+                source: "/:path*"
+            })
+        }
+
+        return headers
+    }
+};
 
 export default nextConfig;
